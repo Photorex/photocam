8b272c004f55e80a47e1fb2f0786461d5c81a949
ecspecial <graffitiraksar55@gmail.com>
2026-01-17 17:27:42 +0300

    updated

--- ecosystem.production.config.js
+++ ecosystem.production.config.js
@@ -55,7 +55,6 @@ module.exports = {
         NODE_ENV: "production",
         TRACE_LOG_DIR: "/var/log/simcam-trace",
         TRACE_MAX_SNIP: "900",
-        NODE_OPTIONS: "--require /var/www/simcam/photo/trace-preload.js"
       },
       
       // Logging
